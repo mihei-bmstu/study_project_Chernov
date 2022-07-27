67ef40c94f47d9f110dfdbddf6f5f4821ca00c92
mvchernov <mvchernov@phoenixit.ru>
2022-07-27 09:59:52 +0300

    Lab7: done

--- src/main/scala/testChernov/Boot.scala
+++ src/main/scala/testChernov/Boot.scala
@@ -1,20 +1,26 @@
 package testChernov
 
+import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
 import testChernov.labs._
 
 object Boot {
   def main(args: Array[String]): Unit = {
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    Logger.getLogger("com").setLevel(Level.ERROR)
+
     val spark = SparkSession.builder()
       .master("local[*]")
       .appName("testChernov")
       .getOrCreate()
 
-    spark.sparkContext.setLogLevel("ERROR")
     import spark.implicits._
 
-    val lab1 = new Lab1(spark)
-    lab1.lab1ReduceByKey()
+/*    val lab1 = new Lab1(spark)
+    lab1.lab1ReduceByKey()*/
+
+    val lab7 = Lab7(spark)
+    lab7.lab7SQLTables()
 
   }
 
---
+++ src/main/scala/testChernov/labs/Lab7.scala
@@ -0,0 +1,63 @@
+package testChernov.labs
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types._
+
+case class Lab7(spark: SparkSession) {
+  def lab7SQLTables(n: Int = 10, trim: Boolean = true): Unit = {
+
+    val customerSchema = StructType(
+        StructField("ID", IntegerType, nullable = false) ::
+        StructField("Name", StringType, nullable = true) ::
+        StructField("Email", StringType, nullable = true) ::
+        StructField("Date", DateType, nullable = true) ::
+        StructField("Status", StringType, nullable = true) ::
+        Nil)
+
+    val productSchema = StructType(
+        StructField("ID", IntegerType, nullable = false) ::
+        StructField("Name", StringType, nullable = true) ::
+        StructField("Price", DoubleType, nullable = true) ::
+        StructField("Number_Of_Products", IntegerType, nullable = true) ::
+        Nil)
+
+    val orderSchema = StructType(
+        StructField("Customer_ID", IntegerType, nullable = false) ::
+        StructField("Order_ID", IntegerType, nullable = false) ::
+        StructField("Product_ID", IntegerType, nullable = false) ::
+        StructField("Number_Of_Products", IntegerType, nullable = true) ::
+        StructField("Order_Date", DateType, nullable = true) ::
+        StructField("Status", StringType, nullable = true) ::
+        Nil)
+
+    val customerDF: Unit = spark.read
+      .options(Map("delimiter" -> "\\t",
+        "header" -> "true",
+        "dateFormat" -> "dd.MM.yyyy"))
+      .schema(customerSchema)
+      .csv("./dataset/customer/customer.csv")
+      .createOrReplaceTempView("customer")
+
+    val orderDF: Unit = spark.read
+      .options(Map("delimiter" -> "\\t",
+        "header" -> "true",
+        "dateFormat" -> "dd.MM.yyyy"))
+      .schema(orderSchema)
+      .csv("./dataset/order/order.csv")
+      .createOrReplaceTempView("order")
+
+    val productDF: Unit = spark.read
+      .options(Map("delimiter" -> "\\t",
+        "header" -> "true",
+        "dateFormat" -> "dd.MM.yyyy"))
+      .schema(productSchema)
+      .csv("./dataset/product/product.csv")
+      .createOrReplaceTempView("product")
+
+    val response: Unit = spark.sql(
+      """
+        |SELECT DISTINCT customer_id, order_date FROM order WHERE status = 'delivered'
+        |""".stripMargin).show(n, trim)
+
+  }
+}

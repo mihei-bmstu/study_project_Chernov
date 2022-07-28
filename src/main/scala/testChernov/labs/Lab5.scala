6a670076e6f5a5c87b9e880590cd987af3fd37be
mvchernov <mvchernov@phoenixit.ru>
2022-07-28 18:09:33 +0300

    Lab 5: done

---
+++ src/main/scala/testChernov/labs/Lab5.scala
@@ -0,0 +1,27 @@
+package testChernov.labs
+
+import org.apache.spark.sql.SparkSession
+import testChernov.system._
+
+case class Lab5(spark: SparkSession) {
+  def lab5GroupBy(): Unit = {
+    println("Lab 5 started")
+    import spark.implicits._
+
+    val orderRDD = spark.read
+      .option("delimiter", "\t")
+      .schema(Parameters.orderSchema)
+      .csv(Parameters.path_order)
+      .as[Order]
+      .rdd
+      .filter(_.Status == "delivered")
+      .map{ case Order(custID, orderID, prodID, numberProd, orderDate, status) =>
+        (custID, (numberProd, 1))}
+      .reduceByKey { case ((v1, c1), (v2, c2)) => (v1 + v2, c1 + c2)}
+      .mapValues { case (v, c) =>  (v, c, v.toDouble / c.toDouble)}
+
+    println("Customer ID, (Sum number of products, number of orders, average order)")
+    orderRDD.foreach(println)
+    println("Lab 5 finished")
+  }
+}

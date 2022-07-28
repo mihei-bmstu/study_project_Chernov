8e868514cf03604b5d5bd35126fa251d9a21cc28
mvchernov <mvchernov@phoenixit.ru>
2022-07-28 10:55:08 +0300

    Lab 1: done

--- src/main/scala/testChernov/labs/Lab1.scala
+++ src/main/scala/testChernov/labs/Lab1.scala
@@ -1,40 +1,33 @@
 package testChernov.labs
 
-import org.apache.spark.rdd._
 import org.apache.spark.sql.SparkSession
 import testChernov.system._
-import java.sql.Date
 
-case class Order(customerID: Int,
-                 orderID: Int,
-                 productID: Int,
-                 numberOfProduct: Int,
-                 orderDate: Date,
-                 status: String)
-
-case class OrderShort(customerID: Int, numberOfProduct: Int)
+case class Order(Customer_ID: String,
+                 Order_ID: Integer,
+                 Product_ID: Integer,
+                 Number_Of_Products: Integer,
+                 Order_Date: String,
+                 Status: String)
 
 class Lab1 (spark: SparkSession) {
   def lab1ReduceByKey(): Unit = {
     println("Lab 1 started")
+    import spark.implicits._
+
     val rddFromFile = spark.read
       .option("delimiter", "\t")
+      .schema(Parameters.orderSchema)
       .csv(Parameters.path_order)
+      .as[Order]
       .rdd
 
     val rddFiltered = rddFromFile
-      .filter(_(5) == "delivered")
-      .map(x=>Order(x(0).asInstanceOf[Int],
-        x(1).asInstanceOf[Int],
-        x(2).asInstanceOf[Int],
-        x(3).asInstanceOf[Int],
-        x(4).asInstanceOf[Date],
-        x(5).asInstanceOf[String])
-      )
-      .map { case Order(x1, x2, x3, x4, x5, x6) => OrderShort(x1, x4) }
+      .filter(_.Status == "delivered")
+      .map { case Order(x1, x2, x3, x4, x5, x6) => (x1, (x4, 1)) }
+      .reduceByKey((x, y) => (x._1 + y._1, x._2 + y._2))
 
     rddFiltered.foreach(println)
-
+    println("Lab 1 finished")
   }
 }
-
